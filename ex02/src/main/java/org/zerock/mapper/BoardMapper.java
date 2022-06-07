4efa0d93e1b5cc819a4f1712dcf7d9457389af7a
naerok <chg9252@gmail.com>
2022-06-07 15:13:42 +0900

    selectKey Test

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,4 +7,14 @@ import org.zerock.domain.BoardVO;
 public interface BoardMapper {
 	
 	List<BoardVO> getList();
+	
+	void insert(BoardVO board);
+	
+	void insertSelectKey(BoardVO board);
+	
+	BoardVO read(Long bno);
+	
+	int delete(Long bno);
+	
+	int update(BoardVO board);
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -14,11 +15,51 @@ import lombok.extern.log4j.Log4j;
 public class BoardMapperTests {
 	
 	@Autowired
-	private BoardMapper boardmapper;
+	private BoardMapper boardMapper;
 	
 	@Test
 	public void testGetList() {
 		log.info("------------------------");
-		boardmapper.getList();
+		boardMapper.getList();
 	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("test 테스트");
+		vo.setContent("content 테스트");
+		vo.setWriter("writer테스트");
+		
+		boardMapper.insert(vo);
+		
+		log.info("------------------------------------");
+		log.info("alter insert" + vo.getBno());
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("test 테스트");
+		vo.setContent("content 테스트");
+		vo.setWriter("writer테스트");
+		
+		boardMapper.insertSelectKey(vo);
+		log.info("------------------------------------");
+		log.info("alter insert selectKey  "+ vo.getBno());
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO vo  = boardMapper.read(3L);
+		log.info(vo);
+	}
+	
+	
+	@Test
+	public void testDelete() {
+		int deleteNum = boardMapper.delete(1L);
+		log.info(deleteNum);
+	}
+	
+	
 }
