ff2e11831b0786eff288683d9e3a3ac51d3d3fac
naerok <chg9252@gmail.com>
2022-06-07 19:30:30 +0900

    lombok재설치 및 오류해결

--- ex02/src/test/java/org/zerock/mapper/TimeMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/TimeMapperTests.java
@@ -7,10 +7,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Log4j
+@Slf4j
 public class TimeMapperTests {
 	
 	@Autowired
